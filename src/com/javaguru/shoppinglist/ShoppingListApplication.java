907fe414575e169874e2ffe2387085b88a79726e
Tawronix <tawronix@gmail.com>
2020-01-26 19:09:30 +0200

    Implementation of Task 1.

--- src/com/javaguru/shoppinglist/ShoppingListApplication.java
+++ src/com/javaguru/shoppinglist/ShoppingListApplication.java
@@ -16,28 +16,65 @@ class ShoppingListApplication {
                 System.out.println("1. Create product");
                 System.out.println("2. Find product by id");
                 System.out.println("3. Exit");
+                System.out.print("Select menu item: ");
                 int userInput = Integer.parseInt(scanner.nextLine());
                 switch (userInput) {
-                    case 1:
-                        System.out.println("Enter product name: ");
-                        String name = scanner.nextLine();
-                        System.out.println("Enter product price: ");
-                        BigDecimal price = new BigDecimal(scanner.nextLine());
+                    case 1: // Create product
                         Product product = new Product();
-                        product.setName(name);
-                        product.setPrice(price);
+                        // Product name
+                        while (true) {
+                            System.out.print("Enter product name: ");
+                            try {
+                                String name = scanner.nextLine();
+                                product.setName(name);
+                                break;
+                            } catch (DataValidationException e) {
+                                System.out.println(e.getMessage() + " Please try again.");
+                            }
+                        }
+                        // Product category
+                        System.out.print("Enter product category: ");
+                        String category = scanner.nextLine();
+                        product.setCategory(category);
+                        // Product price
+                        while (true) {
+                            System.out.print("Enter product price: ");
+                            try {
+                                BigDecimal price = new BigDecimal(scanner.nextLine());
+                                product.setPrice(price);
+                                break;
+                            } catch (DataValidationException | NumberFormatException e) {
+                                System.out.println(e.getMessage() + " Please try again.");
+                            }
+                        }
+                        // Product discount
+                        while (true) {
+                            System.out.print("Enter product discount: ");
+                            try {
+                                BigDecimal discount = new BigDecimal(scanner.nextLine());
+                                product.setDiscount(discount);
+                                break;
+                            } catch (DataValidationException | NumberFormatException e) {
+                                System.out.println(e.getMessage() + " Please try again.");
+                            }
+                        }
+                        // Product description
+                        System.out.print("Enter product description (optional): ");
+                        String description = scanner.nextLine();
+                        product.setDescription(description);
+
                         product.setId(productIdSequence);
                         productRepository.put(productIdSequence, product);
                         productIdSequence++;
-                        System.out.println("Result: " + product.getId());
+                        System.out.printf("New product created successfully. { ID: %d }%n", product.getId());
                         break;
-                    case 2:
-                        System.out.println("Enter product id: ");
+                    case 2: // Find product by id
+                        System.out.print("Enter product id: ");
                         long id = scanner.nextLong();
                         Product findProductResult = productRepository.get(id);
-                        System.out.println(findProductResult);
+                        System.out.println(findProductResult != null? findProductResult : "Product not found.");
                         break;
-                    case 3:
+                    case 3: // Exit
                         return;
                 }
             } catch (Exception e) {
