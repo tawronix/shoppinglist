a2c79fcab525e340a18773934021a0db19f7c6ba
Tawronix <tawronix@gmail.com>
2020-04-13 16:30:25 +0300

    #Task 10 - Hibernate Relationships

--- src/main/java/com/javaguru/shoppinglist/shoppingcart/ProductListItem.java
+++ src/main/java/com/javaguru/shoppinglist/shoppingcart/ProductListItem.java
@@ -2,12 +2,41 @@ package com.javaguru.shoppinglist.shoppingcart;
 
 import com.javaguru.shoppinglist.product.Product;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+@Entity
+@Table(name = "shopping_cart_items")
 public class ProductListItem {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "shopping_cart_id")
+    private ShoppingCart shoppingCart;
+    @OneToOne
+    @JoinColumn(name = "product_id")
     private Product product;
+    @Column(name = "quantity")
     private BigDecimal quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void setShoppingCart(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
 
     public Product getProduct() {
         return product;
@@ -30,12 +59,13 @@ public class ProductListItem {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ProductListItem that = (ProductListItem) o;
-        return Objects.equals(product, that.product) &&
+        return Objects.equals(id, that.id) &&
+                Objects.equals(product, that.product) &&
                 Objects.equals(quantity, that.quantity);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(product, quantity);
+        return Objects.hash(id, product, quantity);
     }
 }
